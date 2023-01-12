de09d5c30984d7b8092ae6f6a097cfe0d7a3546e
Mustafa Aptula (sawyer) <sawyer8604@yahoo.com>
2023-01-12 22:41:34 +0200

    Initial commit

---
+++ Code Formatting/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+
+namespace Code_Formatting
+{    
+    class Program
+    {
+        public const string FILE_NAME = "example.bin";
+        static void Main(string[] args)
+        {
+            FileStream fileStream = new FileStream(FILE_NAME, FileMode.CreateNew);
+
+            // Create the writer for data.
+
+            BinaryWriter writer = new BinaryWriter(fileStream);
+
+            // write data to
+
+            for (int i = 0; i < 11; i++)
+            {
+                writer.Write((int)i);
+            }
+            writer.Close();
+            fileStream.Close();
+
+            // Create the reader for data.
+
+            fileStream = new FileStream(FILE_NAME, FileMode.Open, FileAccess.Read);
+
+            BinaryReader reader = new BinaryReader(fileStream);
+
+            // Read data from Test.data.
+
+            for (int i = 0; i < 11; i++)
+            {
+                Console.WriteLine(reader.ReadInt32());
+            }
+
+            reader.Close();
+            fileStream.Close();
+        }
+    }
+}

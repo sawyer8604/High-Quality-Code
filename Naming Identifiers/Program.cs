ef4fd8bf23e14a33a7fa91b37fd4fd3d0408a2b2
Mustafa Aptula (sawyer) <sawyer8604@yahoo.com>
2023-01-13 16:47:03 +0200

    Refactor the following examples to produce code with well-named C# identifiers

---
+++ Naming Identifiers/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Naming_Identifiers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Numbers numbers = new Numbers();
+        }
+    }
+
+    public class Numbers
+    {
+        const int MaxCount = 6;
+        class InnerClasNumbers
+        {
+            internal void Print(bool variable)
+            {
+                string variableAsString = variable.ToString();
+                Console.WriteLine(variableAsString);
+            }
+        }
+        public void Entrance()
+        {
+            InnerClasNumbers instance = new InnerClasNumbers();
+
+            instance.Print(true);
+        }
+    }
+}
